1aadf71e4338469c51ecc59458ade5ecc42cf05e
AdamSzentesi <adam.szentesi@google.com>
2017-12-12 17:32:59 +0100

    Terrain generation OK

--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class World : MonoBehaviour
 {
+	private string X = "X";
+	private string Y = "Y";
+	private string Z = "Z";
+
 	public GameObject player;
 	private Transform playerTransform;
 	private Vector3i playerChunkPosition; //players chunk position (chunk units)
 
 	private int seed = 666; //default world seed
-	private int viewDistance = 1; //Moore neighborhood for chunk generation (chunk units)
+	private int viewDistance = 4; //Moore neighborhood for chunk generation (chunk units)
 	private int chunkSize = 16; //default chunk size (block units)
 	private GameObject[,] chunks;
 	public Dictionary<Vector3i, Dictionary<Vector3i, byte>> changedBlocks = new Dictionary<Vector3i, Dictionary<Vector3i, byte>>(); //DB of changed blocks: saving, loading and redrawing
@@ -35,7 +39,10 @@ public class World : MonoBehaviour
 			this.changedBlocks = saveData.GetChangedBlocks();
 			this.playerTransform.position = new Vector3(saveData.playerPositionX, saveData.playerPositionY, saveData.playerPositionZ);
 			this.playerChunkPosition = GetChunkPosition(this.playerTransform.position);
-			this.player.GetComponent<Inventory> ().Populate (saveData.inventoryItemCounts, saveData.inventoryItems);
+			if(saveData.inventoryItemCounts != null)
+			{
+				this.player.GetComponent<Inventory> ().Populate (saveData.inventoryItemCounts, saveData.inventoryItems);	
+			}
 		}
 
 		//collider setup
@@ -64,18 +71,9 @@ public class World : MonoBehaviour
 
 		Vector3i currentChunkPosition = GetChunkPosition(this.playerTransform.position);
 
-		if(currentChunkPosition.x != this.playerChunkPosition.x)
-		{
-			this.playerChunkPosition.Set(currentChunkPosition);
-			ScrollChunks (this.playerChunkPosition.x - currentChunkPosition.x);
-		}
-
-//		if (!currentChunkPosition.Equals(this.playerChunkPosition))
-//		{
-//			print ("POS CHANGE");
-//			this.playerChunkPosition.set (currentChunkPosition);
-//		}
-//
+		ScrollChunks (currentChunkPosition.x - this.playerChunkPosition.x, this.X);
+		ScrollChunks (currentChunkPosition.z - this.playerChunkPosition.z, this.Z);
+		this.playerChunkPosition.Set(currentChunkPosition);
 	}
 
 	//create chunk (chunk units)
@@ -115,18 +113,21 @@ public class World : MonoBehaviour
 	//update colliders of certain chunk (array units)
 	private void UpdateColliders(int x, int z)
 	{
-		List<Vector3i> colliderVoxels = this.chunks[x, z].GetComponent<Chunk>().GetColliderVoxels(playerTransform.position, this.colliderDistance);
-		Vector3i chunkWorldPosition = this.chunks[x, z].GetComponent<Chunk>().worldPosition;
-		for (int i = 0; i < colliderVoxels.Count; i++)
+		if (this.chunks [x, z] != null)
 		{
-			Vector3i blockPosition = colliderVoxels[i];
-			Vector3i worldPosition = blockPosition.Add (chunkWorldPosition);
-			GameObject newCollider = new GameObject();
-			this.blockColliders.Add (newCollider);
-			newCollider.name = (x.ToString() + "_" + z.ToString() + "_" + i.ToString());
-			newCollider.AddComponent<BoxCollider>();
-			newCollider.transform.position = new Vector3(worldPosition.x, worldPosition.y, worldPosition.z);
-			newCollider.transform.SetParent (this.terrainCollider.transform);
+			List<Vector3i> colliderVoxels = this.chunks[x, z].GetComponent<Chunk>().GetColliderVoxels(playerTransform.position, this.colliderDistance);
+			Vector3i chunkWorldPosition = this.chunks[x, z].GetComponent<Chunk>().worldPosition;
+			for (int i = 0; i < colliderVoxels.Count; i++)
+			{
+				Vector3i blockPosition = colliderVoxels[i];
+				Vector3i worldPosition = blockPosition.Add (chunkWorldPosition);
+				GameObject newCollider = new GameObject();
+				this.blockColliders.Add (newCollider);
+				newCollider.name = (x.ToString() + "_" + z.ToString() + "_" + i.ToString());
+				newCollider.AddComponent<BoxCollider>();
+				newCollider.transform.position = new Vector3(worldPosition.x, worldPosition.y, worldPosition.z);
+				newCollider.transform.SetParent (this.terrainCollider.transform);
+			}
 		}
 	}
 
@@ -140,29 +141,110 @@ public class World : MonoBehaviour
 		return result;
 	}
 
-	//TODO: this! :)
-	private void ScrollChunks(int amount)
+	private void ScrollChunks(int amount, string axis) //+-2
 	{
-		int max = this.viewDistance * 2 + 1;
-
-		for (int z = 0; z < max; z++)
+		int signum = (int)Mathf.Sign (amount);
+		switch (signum)
 		{
-			Destroy (this.chunks[0, z]);
-		}
-
-		for (int x = 1; x < max; x++)
-		{
-			for (int z = 0; z < max; z++)
+			case 1:
 			{
-				this.chunks [x - 1, z] = this.chunks[x, z];
+				int endMax = this.chunks.GetLength (0); //5
+				int endMin = 0 + amount; //2
+				endMin = Clamp (endMin, 0, endMax); //2
+
+				//destroy unused chunks
+				for (int i = 0; i < endMin; i++){DestroyChunks (i, axis);}
+				//move keeped chunks
+				for (int i = endMin; i < endMax; i++){MoveChunks (i, axis, amount);}
+				//create new Chunks
+				for (int i = (endMax - endMin); i < endMax; i++){CreateChunks (i, axis, amount);}
+				break;
+			}
+			case -1:
+			{
+				int endMax = this.chunks.GetLength (0); //5
+				int startMin = 0; //0
+				int startMax = endMax + amount; //3
+				startMax = Clamp(startMax, startMin, endMax); //3
+
+				//destroy unused chunks
+				for (int i = startMax; i < endMax; i++){DestroyChunks (i, axis);}
+				//move keeped chunks
+				for (int i = startMax - 1; i > startMin - 1; i--){MoveChunks (i, axis, amount);}
+				//create new Chunks
+				for (int i = (endMax - startMax - 1); i > startMin - 1; i--){CreateChunks (i, axis, amount);}
+				break;
 			}
 		}
+	}
 
-		for (int z = 0; z < max; z++)
+	private void CreateChunks(int axisRow, string axis, int amount)
+	{
+		for (int j = 0; j < this.chunks.GetLength (0); j++)
 		{
-			CreateChunk (max - 1, z);
+			switch (axis)
+			{
+			case "X":
+				{
+					this.chunks[axisRow, j] = CreateChunk(axisRow + playerChunkPosition.x - this.viewDistance + amount, j + playerChunkPosition.z - this.viewDistance);
+					break;
+				}
+			case "Z":
+				{
+					this.chunks[j, axisRow] = CreateChunk(j + playerChunkPosition.x - this.viewDistance, axisRow + playerChunkPosition.z - this.viewDistance + amount);
+					break;
+				}
+			}
 		}
+	}
 
+
+	private void DestroyChunks(int axisRow, string axis)
+	{
+		for (int j = 0; j < this.chunks.GetLength (0); j++)
+		{
+			switch (axis)
+			{
+			case "X":
+				{
+					Destroy (this.chunks[axisRow, j]);
+					break;
+				}
+			case "Z":
+				{
+					Destroy (this.chunks[j, axisRow]);
+					break;
+				}
+			}
+		}
+	}
+
+	private void MoveChunks(int axisRow, string axis, int amount)
+	{
+		for (int j = 0; j < this.chunks.GetLength (0); j++)
+		{
+			switch (axis)
+			{
+			case "X":
+				{
+					this.chunks [axisRow - amount, j] = this.chunks [axisRow, j];
+					break;
+				}
+			case "Z":
+				{
+					this.chunks [j, axisRow - amount] = this.chunks [j, axisRow];
+					break;
+				}
+			}
+		}
+	}
+
+	private int Clamp(int number, int clampMin, int clampMax)
+	{
+		int result = number;
+		result = Mathf.Max (result, clampMin);
+		result = Mathf.Min (result, clampMax);
+		return result;
 	}
 
 	//digs a block
